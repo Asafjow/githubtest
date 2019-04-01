7e54517ff2373a0eaa1bf75efbf5245e198f3a06
Asaf The king <AsafSa@bezeqint.co.il>
2019-04-01 11:31:00 +0300

    asafphp

---
+++ phptest.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>My First PHP Page</title>
+  </head>
+  <body>
+  <?php   
+    echo "Hello World!";
+	 $name = 'John';
+   $age = 25;
+   echo $name;
+  ?>
+  </body>
+</html>
+
